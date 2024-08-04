89babf9e8ea1d598ef2cca2a30c5c5b3b0c26880
Elsman <ahmedelsman0@gmail.com>
2024-08-04 10:07:22 +0300

    first commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ahmed Elsman on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
