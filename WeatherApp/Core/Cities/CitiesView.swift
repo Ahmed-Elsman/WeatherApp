45e781cb7e8352de99a1a4151ab5c09f45cf7314
Elsman <ahmedelsman0@gmail.com>
2024-08-04 10:24:00 +0300

    [Feature] create cities view for showing of list of added cities

---
+++ WeatherApp/Core/Cities/CitiesView.swift
@@ -0,0 +1,18 @@
+//
+//  CitiesView.swift
+//  WeatherApp
+//
+//  Created by Ahmed Elsman on 04/08/2024.
+//
+
+import SwiftUI
+
+struct CitiesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CitiesView()
+}
