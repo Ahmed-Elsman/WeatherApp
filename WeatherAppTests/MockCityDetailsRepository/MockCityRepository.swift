eb9bbc26eef7f870daaaf1a18c74e6a56e703da9
Elsman <ahmedelsman0@gmail.com>
2024-08-05 22:35:02 +0300

    [Test] adding tests for CityDetailsViewModel also adding MockCityReposatory

---
+++ WeatherAppTests/MockCityDetailsRepository/MockCityRepository.swift
@@ -0,0 +1,20 @@
+//
+//  MockCityRepository.swift
+//  WeatherAppTests
+//
+//  Created by Ahmed Elsman on 05/08/2024.
+//
+
+@testable import WeatherApp
+
+class MockCityRepository: CityRepositoryProtocol {
+    var mockCity: City?
+    var shouldThrowError = false
+    
+    func fetchCityWeather(cityName: String) async throws -> City {
+        if shouldThrowError {
+            throw NSError(domain: "TestError", code: 1, userInfo: [NSLocalizedDescriptionKey: "Test error message"])
+        }
+        return mockCity!
+    }
+}
